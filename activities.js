f16593a2eddc06c52ec83d12011dbd9301795a27
Satish Sampath <ssm@fb.com>
2018-10-28 22:43:47 -0700

    Added alerts to guide new users. Also renamed this tool to 'Activities'

--- activities.js
+++ activities.js
@@ -5,18 +5,38 @@ var finishActivitySound = new Howl({
 
 function addActivityToList(name, mins) {
   mins = validateActivityDuration(mins);
-  $("#playlist-listgroup").append(
+  var item = $(
     "<a class='list-group-item list-group-item-action'>" + name + 
         "<input size='6' class='ml-5 mr-1' type='text' value='" + mins + "'>" +
         "<label>mins</label>" +
         "<button type='button' class='close' aria-label='Close'>" +
         "<span aria-hidden='true'>&times;</span>" +
         "</button>" +
-    "</a>");
+    "</a>").appendTo("#activities-listgroup");;
+
+  item.click(function (e) {
+    e.preventDefault();
+    if (!isEditingActivities()) {
+      var name = $(this).contents().get(0).nodeValue;
+      startActivity(name);
+    }
+  }).children().click(function(e) {
+    return false;
+  });
+
+  item.find("button.close").on("click", function(e) {
+    e.preventDefault();
+    var name = $(this).parent().contents().get(0).nodeValue;
+    if (window.confirm("Do you want to delete the activity '" + name + "'?")) {
+      store.remove("activity-" + name);
+      removeActivityFromList(name);
+    }
+    return false;  // Don't invoke list item's click handler.
+  });
 }
 
 function removeActivityFromList(name) {
-  $("#playlist-listgroup a.list-group-item:contains('" + name + "')").remove();
+  $("#activities-listgroup a.list-group-item:contains('" + name + "')").remove();
 }
 
 function iterateAllActivities(fn) {
@@ -36,32 +56,46 @@ function validateActivityDuration(val) {
 }
 
 function editActivities() {
+  if (isDoingActivity())
+    return;  // Don't do anything if user is on a timer now.
+
+  $("#activityListEmptyAlert").hide();
   $("#editActivities").hide();
   $("#finishedEditActivities").show();
   $("#addNewActivityForm").show();
-  $("#playlist-listgroup input").show();
-  $("#playlist-listgroup label").show();
-  $("#playlist-listgroup button.close").show();
+  $("#activities-listgroup input").show();
+  $("#activities-listgroup label").show();
+  $("#activities-listgroup button.close").show();
 }
 
 function finishedEditActivities() {
+  $("#activityAddedAlert").hide();
   $("#editActivities").show();
   $("#finishedEditActivities").hide();
   $("#addNewActivityForm").hide();
-  $("#playlist-listgroup input").hide();
-  $("#playlist-listgroup label").hide();
-  $("#playlist-listgroup button.close").hide();
+  $("#activities-listgroup input").hide();
+  $("#activities-listgroup label").hide();
+  $("#activities-listgroup button.close").hide();
   iterateAllActivities(function(name, value) {
-    var mins = $("#playlist-listgroup a:contains('" + name + "') > input").val();
+    var mins = $("#activities-listgroup a:contains('" + name + "') > input").val();
     mins = validateActivityDuration(mins);
     value.duration = mins;
-    $("#playlist-listgroup a:contains('" + name + "') > input").val(mins);
+    $("#activities-listgroup a:contains('" + name + "') > input").val(mins);
     store.set("activity-" + name, value);
   });
+  if ($("#activities-listgroup").children().length == 0) {
+    $("#activityListEmptyAlert").show();
+  } else {
+    $("#activityListEmptyAlert").hide();
+  }
 }
 
 function isEditingActivities() {
   return $("#finishedEditActivities").css('display') != 'none';
+}
+
+function isDoingActivity() {
+  return $("#clockContainer").css('display') != 'none';
 }
 
 function addNewActivity() {
@@ -72,12 +106,14 @@ function addNewActivity() {
   addActivityToList(name, mins);
   $("#inputNewActivity").val("");
   $("#inputNewActivityTime").val("");
+  $("#activityAddedAlert").show();
   return false;
 }
 
 function startActivity(name) {
   $("#activityListContainer").hide();
   $("#clockContainer").show();
+  $("#clockContainer #title").text(name);
   var mins = store.get("activity-" + name).duration;
   mins = validateActivityDuration(mins);
   initStopWatch("clockDiv",
@@ -104,30 +140,11 @@ function setup() {
   iterateAllActivities(function(name, value) {
     addActivityToList(name, value.duration);
   });
-  $("#playlist-listgroup button.close").on("click", function(e) {
-    e.preventDefault();
-    var name = $(this).parent().contents().get(0).nodeValue;
-    if (window.confirm("Do you want to delete the activity '" + name + "'?")) {
-      store.remove("activity-" + name);
-      removeActivityFromList(name);
-    }
-    return false;  // Don't invoke list item's click handler.
-  })
-  $("#playlist-listgroup a").click(function (e) {
-    e.preventDefault();
-    if (!isEditingActivities()) {
-      var name = $(this).contents().get(0).nodeValue;
-      startActivity(name);
-    }
-  }).children().click(function(e) {
-    return false;
-  });
   $("#finishActivity").click(function(e) {
     e.preventDefault();
     finishActivity(true);
   })
   finishedEditActivities();
-  finishActivity(true);
 }
 
 $(document).ready(setup);
